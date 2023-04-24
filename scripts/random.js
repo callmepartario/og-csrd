5d0c549eef7dedf87e7964fef26dca0385f323f2
Partario Flynn <partario.flynn@gmail.com>
2023-04-24 12:29:50 -0400

    chargen add

--- scripts/random.js
+++ scripts/random.js
@@ -4,16 +4,26 @@ let data = await fetchData.json();
 
 generateOddity();
 
-// Functions
+// Random Character
+function randomcharactersentence() {
+  let characterdescriptor = data.characterdescriptors[Math.floor(Math.random() * data.characterdescriptors.length)];
+  let charactertype = data.charactertypes[Math.floor(Math.random() * data.charactertypes.length)];;
+  let characterfocus = data.characterfoci[Math.floor(Math.random() * data.characterfoci.length)];;
+  let randomcharactersentence = characterdescriptor + " " + charactertype + " who " + characterfocus;
+  document.getElementById("random-character-sentence").innerHTML = randomcharactersentence;
+}
+// Random Oddity
 function generateOddity() {
   let oddityquality = data.oddityqualities[Math.floor(Math.random() * data.oddityqualities.length)];
   let oddityproperty = data.oddityproperties[Math.floor(Math.random() * data.oddityproperties.length)];;
   let oddityform = data.oddityforms[Math.floor(Math.random() * data.oddityforms.length)];;
   let randomoddity = oddityquality + " " + oddityproperty + " " + oddityform;
-
-  // Send oddity to HTML
   document.getElementById("random-oddity").innerHTML = randomoddity;
 }
+
+document.getElementById('random-character-sentence-button').addEventListener('click', function (event) {
+  generateOddity();
+});
 
 document.getElementById('random-oddity-button').addEventListener('click', function (event) {
   generateOddity();
