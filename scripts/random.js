7afe335c772402ae6f645eeac8d8075b6a9b709c
Partario Flynn <partario.flynn@gmail.com>
2023-04-24 14:20:32 -0400

    button no worky?

--- scripts/random.js
+++ scripts/random.js
@@ -2,31 +2,33 @@
 let fetchData = await fetch("./db/db.json");
 let data = await fetchData.json();
 
+//initialize
 generateCharacterSentence();
 generateOddity();
 
 // Random Character
 function generateCharacterSentence() {
-  let characterdescriptor = data.characterdescriptorlist[Math.floor(Math.random() * data.characterdescriptorlist.length)];
-  let charactertype = data.charactertypelist[Math.floor(Math.random() * data.charactertypelist.length)];;
-  let characterfocus = data.characterfocuslist[Math.floor(Math.random() * data.characterfocuslist.length)];;
-  let charactersentence = "I am " + characterdescriptor + " " + charactertype + " who " + characterfocus + ".";
-  document.getElementById("generated-character-sentence").innerHTML = charactersentence;
+  let descriptor = data.characterdescriptorlist[Math.floor(Math.random() * data.characterdescriptorlist.length)];
+  let type = data.charactertypelist[Math.floor(Math.random() * data.charactertypelist.length)];;
+  let focus = data.characterfocuslist[Math.floor(Math.random() * data.characterfocuslist.length)];;
+  let sentence = "I am " + descriptor + " " + type + " who " + focus + ".";
+  document.getElementById("generated-character-sentence").innerHTML = sentence;
 }
 // Random Oddity
 function generateOddity() {
-  let oddityquality = data.oddityqualitylist[Math.floor(Math.random() * data.oddityqualitylist.length)];
-  let oddityproperty = data.odditypropertylist[Math.floor(Math.random() * data.odditypropertylist.length)];;
-  let oddityform = data.oddityformlist[Math.floor(Math.random() * data.oddityformlist.length)];;
-  let oddity = oddityquality + " " + oddityproperty + " " + oddityform;
+  let quality = data.oddityqualitylist[Math.floor(Math.random() * data.oddityqualitylist.length)];
+  let property = data.odditypropertylist[Math.floor(Math.random() * data.odditypropertylist.length)];;
+  let form = data.oddityformlist[Math.floor(Math.random() * data.oddityformlist.length)];;
+  let oddity = quality + " " + property + " " + form;
   document.getElementById("random-oddity").innerHTML = oddity;
 }
 
 // Listen
 document.getElementById("generate-new-character-sentence").addEventListener("click", function (event) {
-  generateCharacterSentence();
+  generateCharacterSentences();
 });
 
 document.getElementById("generate-new-random-oddity").addEventListener("click", function (event) {
   generateOddity();
 });
+
