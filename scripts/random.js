7dd1443da67991a4d999375fa0aac174b8f54f1f
Partario Flynn <partario.flynn@gmail.com>
2023-04-24 12:36:41 -0400

    Add files via upload

--- scripts/random.js
+++ scripts/random.js
@@ -5,11 +5,11 @@ let data = await fetchData.json();
 generateOddity();
 
 // Random Character
-function randomcharactersentence() {
+function generateCharacterSentence() {
   let characterdescriptor = data.characterdescriptors[Math.floor(Math.random() * data.characterdescriptors.length)];
   let charactertype = data.charactertypes[Math.floor(Math.random() * data.charactertypes.length)];;
   let characterfocus = data.characterfoci[Math.floor(Math.random() * data.characterfoci.length)];;
-  let randomcharactersentence = characterdescriptor + " " + charactertype + " who " + characterfocus;
+  let randomcharactersentence = "I am a " + characterdescriptor + " " + charactertype + " who " + characterfocus; + "."
   document.getElementById("random-character-sentence").innerHTML = randomcharactersentence;
 }
 // Random Oddity
@@ -17,14 +17,14 @@ function generateOddity() {
   let oddityquality = data.oddityqualities[Math.floor(Math.random() * data.oddityqualities.length)];
   let oddityproperty = data.oddityproperties[Math.floor(Math.random() * data.oddityproperties.length)];;
   let oddityform = data.oddityforms[Math.floor(Math.random() * data.oddityforms.length)];;
-  let randomoddity = oddityquality + " " + oddityproperty + " " + oddityform;
+  let randomoddity = oddityquality + " " + oddityproperty + " " + oddityform; +
   document.getElementById("random-oddity").innerHTML = randomoddity;
 }
 
-document.getElementById('random-character-sentence-button').addEventListener('click', function (event) {
-  generateOddity();
+document.getElementById("random-character-sentence-button").addEventListener("click", function (event) {
+  generateCharacterSentence();
 });
 
-document.getElementById('random-oddity-button').addEventListener('click', function (event) {
+document.getElementById("random-oddity-button").addEventListener("click", function (event) {
   generateOddity();
 });
