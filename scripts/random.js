619d0a19d7d8de93cb9ce7b0c9f3befaaa8ca6e0
Partario Flynn <partario.flynn@gmail.com>
2023-04-24 14:40:56 -0400

    character generator independent fields

--- scripts/random.js
+++ scripts/random.js
@@ -3,32 +3,51 @@ let fetchData = await fetch("./db/db.json");
 let data = await fetchData.json();
 
 //initialize
-generateCharacterSentence();
-generateOddity();
+randomCharacterDescriptor();
+randomCharacterType();
+randomCharacterFocus();
+randomOddity();
 
-// Random Character
-function generateCharacterSentence() {
+// Random Character Descriptor, Type and Focus
+function randomCharacterDescriptor() {
   let descriptor = data.characterdescriptorlist[Math.floor(Math.random() * data.characterdescriptorlist.length)];
-  let type = data.charactertypelist[Math.floor(Math.random() * data.charactertypelist.length)];;
-  let focus = data.characterfocuslist[Math.floor(Math.random() * data.characterfocuslist.length)];;
-  let sentence = "I am " + descriptor + " " + type + " who " + focus + ".";
-  document.getElementById("generated-character-sentence").innerHTML = sentence;
+  document.getElementById("character-random-descriptor").innerHTML = descriptor;
 }
+function randomCharacterType() {
+  let type = data.characterdescriptorlist[Math.floor(Math.random() * data.characterdescriptorlist.length)];
+  document.getElementById("character-random-type").innerHTML = type;
+}
+function randomCharacterFocus() {
+  let focus = data.characterdescriptorlist[Math.floor(Math.random() * data.characterdescriptorlist.length)];
+  document.getElementById("character-random-focus").innerHTML = focus;
+}
+
 // Random Oddity
-function generateOddity() {
+function randomOddity() {
   let quality = data.oddityqualitylist[Math.floor(Math.random() * data.oddityqualitylist.length)];
   let property = data.odditypropertylist[Math.floor(Math.random() * data.odditypropertylist.length)];;
   let form = data.oddityformlist[Math.floor(Math.random() * data.oddityformlist.length)];;
   let oddity = quality + " " + property + " " + form;
-  document.getElementById("random-oddity").innerHTML = oddity;
+  document.getElementById("new-random-oddity").innerHTML = oddity;
 }
 
 // Listen
-document.getElementById("generate-new-character-sentence").addEventListener("click", function (event) {
-  generateCharacterSentences();
+document.getElementById("new-random-character-descriptor").addEventListener("click", function (event) {
+  randomCharacterDescriptor();
+});
+document.getElementById("new-random-character-type").addEventListener("click", function (event) {
+  randomCharacterType();
+});
+document.getElementById("new-random-character-focus").addEventListener("click", function (event) {
+  randomCharacterType();
+});
+document.getElementById("new-random-character-sentence").addEventListener("click", function (event) {
+  randomCharacterDescriptor();
+  randomCharacterType();
+  randomCharacterFocus();  
 });
 
 document.getElementById("generate-new-random-oddity").addEventListener("click", function (event) {
-  generateOddity();
+  randomOddity();
 });
 
