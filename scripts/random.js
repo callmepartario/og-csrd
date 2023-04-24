d648858c7c47cab34b0d4b676fdcbd5b29ab65b7
Partario Flynn <partario.flynn@gmail.com>
2023-04-24 14:04:10 -0400

    Add files via upload

--- scripts/random.js
+++ scripts/random.js
@@ -2,6 +2,7 @@
 let fetchData = await fetch("./db/db.json");
 let data = await fetchData.json();
 
+generateCharacterSentence();
 generateOddity();
 
 // Random Character
@@ -9,16 +10,16 @@ function generateCharacterSentence() {
   let characterdescriptor = data.characterdescriptorlist[Math.floor(Math.random() * data.characterdescriptorlist.length)];
   let charactertype = data.charactertypelist[Math.floor(Math.random() * data.charactertypelist.length)];;
   let characterfocus = data.characterfocuslist[Math.floor(Math.random() * data.characterfocuslist.length)];;
-  let randomcharactersentence = "I am a(n) <em>" + characterdescriptor + " " + charactertype + "</em> who <em>" + characterfocus + "</em>.";
-  document.getElementById("random-character-sentence").innerHTML = randomcharactersentence;
+  let charactersentence = "I am " + characterdescriptor + " " + charactertype + " who " + characterfocus + ".";
+  document.getElementById("random-character-sentence").innerHTML = charactersentence;
 }
 // Random Oddity
 function generateOddity() {
   let oddityquality = data.oddityqualitylist[Math.floor(Math.random() * data.oddityqualitylist.length)];
   let oddityproperty = data.odditypropertylist[Math.floor(Math.random() * data.odditypropertylist.length)];;
   let oddityform = data.oddityformlist[Math.floor(Math.random() * data.oddityformlist.length)];;
-  let randomoddity = oddityquality + " " + oddityproperty + " " + oddityform;
-  document.getElementById("random-oddity").innerHTML = randomoddity;
+  let oddity = oddityquality + " " + oddityproperty + " " + oddityform;
+  document.getElementById("random-oddity").innerHTML = oddity;
 }
 
 document.getElementById("random-character-sentence-button").addEventListener("click", function (event) {
