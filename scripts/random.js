a35e5c70e0e591b681916ec285a643204386e341
Partario Flynn <partario.flynn@gmail.com>
2023-04-24 12:02:27 -0400

    Add files via upload

---
+++ scripts/random.js
@@ -0,0 +1,20 @@
+// Fetch data
+let fetchData = await fetch("./db/db.json");
+let data = await fetchData.json();
+
+generateOddity();
+
+// Functions
+function generateOddity() {
+  let oddityquality = data.oddityqualities[Math.floor(Math.random() * data.oddityqualities.length)];
+  let oddityproperty = data.oddityproperties[Math.floor(Math.random() * data.oddityproperties.length)];;
+  let oddityform = data.oddityforms[Math.floor(Math.random() * data.oddityforms.length)];;
+  let oddity = oddityquality + " " + oddityproperty + " " + oddityform;
+
+  // Send oddity to HTML
+  document.getElementById("randomoddity").innerHTML = random-oddity;
+}
+
+document.getElementById('random-oddity-button').addEventListener('click', function (event) {
+  generateOddity();
+});
