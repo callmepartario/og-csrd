f502f47d641cf45a6ca6b66b13b69bf35dee1657
Mathew Brookes <83518330+Zantigoo@users.noreply.github.com>
2023-06-13 00:10:17 -0400

    ServiceWorker now network first

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-const cacheName = 'OG-CSRD';
 const cacheFiles = [
     'index.html',
     'og-dd.html',
@@ -13,30 +12,69 @@ const cacheFiles = [
     'icon/apple-touch-icon.png',
     'icon/favicon.ico'
 ]
+// the cache version gets updated every time there is a new deployment
+const CACHE_VERSION = 1;
+const CURRENT_CACHE = `OG-CSRD-${CACHE_VERSION}`;
+console.log(CURRENT_CACHE)
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        (async () => {
-            const cache = await caches.open(cacheName);
-            console.log("[Service Worker] Caching all: app shell and content");
-            await cache.addAll(cacheFiles)
+// on activation we clean up the previously registered service workers
+self.addEventListener('activate', evt =>
+  evt.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
+          }
         })
-    )
-})
+      );
+    })
+  )
+);
 
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        (async () => {
-            const r = await caches.match(e.request);
-            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-            if (r) {
-                return r;
-            }
-            const response = await fetch(e.request);
-            const cache = await caches.open(cacheName);
-            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-            cache.put(e.request, response.clone());
-            return response;
-        })()
+// on install we download the routes we want to cache for offline
+self.addEventListener('install', evt =>
+  evt.waitUntil(
+    caches.open(CURRENT_CACHE).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  )
+);
+
+// fetch the resource from the network
+const fromNetwork = (request, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      update(request);
+    }, reject);
+  });
+
+// fetch the resource from the browser cache
+const fromCache = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      cache
+        .match(request)
+        .then(matching => matching)
     );
+
+// cache the current page to make it available for offline
+const update = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      fetch(request).then(response => cache.put(request, response))
+    );
+
+// general strategy when making a request (eg if online try to fetch it
+// from the network with a timeout, if something fails serve from cache)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
+  );
+  evt.waitUntil(update(evt.request));
 });
